e6c5e1f6f7ace7324dfd8e1869119e4179bd6122
Mark Pham <minhpt10595@gmail.com>
2023-01-16 18:28:00 +0700

    Update Excel Template

--- src/main/java/com/vatek/hrmtool/mapping/excel/UserExcelMapping.java
+++ src/main/java/com/vatek/hrmtool/mapping/excel/UserExcelMapping.java
@@ -1,19 +1,11 @@
 package com.vatek.hrmtool.mapping.excel;
 
-import com.vatek.hrmtool.dto.timesheet.TimesheetExcelDto;
 import com.vatek.hrmtool.dto.user.UserExcelDto;
-import com.vatek.hrmtool.entity.TimesheetEntity;
 import com.vatek.hrmtool.entity.UserEntity;
-import com.vatek.hrmtool.enumeration.TimesheetType;
-import com.vatek.hrmtool.mapping.TimesheetMapping;
 import com.vatek.hrmtool.mapping.common.EntityMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-
-import java.awt.font.TextMeasurer;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",uses = {TimesheetExcelMapping.class})
 public interface UserExcelMapping extends EntityMapper<UserExcelDto, UserEntity> {
@@ -24,8 +16,4 @@ public interface UserExcelMapping extends EntityMapper<UserExcelDto, UserEntity>
             @Mapping(target = "overtimeHours",source = "overtimeHours")
     })
     UserExcelDto toDto(UserEntity entity);
-
-    default List<TimesheetEntity> mapTimesheetToDto(UserEntity entity, TimesheetType timesheetType){
-        return entity.getTimesheetEntities().stream().filter(x -> x.getTimesheetType() == timesheetType).toList();
-    }
 }
